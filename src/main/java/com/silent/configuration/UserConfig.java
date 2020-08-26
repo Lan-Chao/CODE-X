95fe9300e95574f2ea571a9bce4175e6d6a9ed06
Lan-Chao <1515837280@qq.com>
2020-08-26 22:29:56 +0800

    condition  demo

--- src/main/java/com/silent/configuration/UserConfig.java
+++ src/main/java/com/silent/configuration/UserConfig.java
@@ -3,6 +3,7 @@ package com.silent.configuration;
 
 import com.silent.demo.RedisConditionDemo;
 import com.silent.model.Person;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class UserConfig {
 
+    /**
+     *   eg:  @ConditionalOnClass(name = "com.silent.controller.TestController")
+     *
+     * */
     @Bean
     @Conditional(RedisConditionDemo.class)
     public Person person(){
