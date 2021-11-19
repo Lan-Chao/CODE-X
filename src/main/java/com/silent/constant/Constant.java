b25e7906cffd56f4956d994392e37f73da901e75
zhaochangren <zhaochangren@didachuxing.com>
2021-11-19 10:32:49 +0800

    Constant

---
+++ src/main/java/com/silent/constant/Constant.java
@@ -0,0 +1,13 @@
+package com.silent.constant;
+
+/**
+ * @author zhaoc
+ * @date 2021/11/19 9:54
+ */
+public class Constant {
+
+    public final static String HOST = "localhost";
+
+    public final static Integer PORT = 8080;
+
+}
